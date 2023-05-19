061ac820d1e806de32d0903492f05340f5a7d8ae
SieR-VR <nwh040512@gmail.com>
2023-05-19 17:58:34 +0900

    fix module path

--- parsers/functionCall.ts
+++ parsers/functionCall.ts
@@ -1,6 +1,6 @@
 import { makeParseRuleModule } from "infinite-lang/rule/parser";
 
-export default makeParseRuleModule({ role: "expression", nodeType: "functionCall", priority: 0 }, [
+export default makeParseRuleModule({ role: "expression", nodeType: "functionCall", priority: 1 }, [
     {
         role: "identifier",
         condition: () => true,
