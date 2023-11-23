e4492f43e2d7c27c25a9fd01e9afeff81fc307d5
SieR-VR <nwh040512@gmail.com>
2023-11-24 08:52:56 +0900

    fix: delete dependency of ast node

--- src/nn_ast/source.py
+++ src/nn_ast/source.py
@@ -2,15 +2,12 @@ from node import Node
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from source_file import SourceFile
     from nn_ast.statement import Statement
 
 
 class Source(Node):
-    stmts: list["Statement"]
-
     def __init__(
-        self, stmts: list["Statement"], source: "SourceFile", *args, **kwargs
+        self, stmts: list["Statement"], *args, **kwargs
     ) -> None:
-        super().__init__(source, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.stmts = stmts
