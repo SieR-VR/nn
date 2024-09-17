e1a171b263767415cd0e2e910fdd415ee3bc34eb
SieR-VR <nwh040512@gmail.com>
2024-09-17 19:21:29 +0900

    feat: flow name resolve and check circular dependency

--- packages/nn-type-checker/src/resolver/index.ts
+++ packages/nn-type-checker/src/resolver/index.ts
@@ -1,8 +1,14 @@
 import { Declaration, Identifier, isCallExpression, isIdentifierExpression, travel } from "nn-language";
-import { Result, Ok, Err } from "ts-features";
+import { Result, Ok, Err, Option, Some, None } from "ts-features";
 
-import { DeclarationScope, FileScope, Size, Value, ResolveError } from "./types";
-	
+import { DeclarationScope, FileScope, Size, Value, ResolveError, Flow } from "./types";
+
+const defaultFlows: Record<string, Flow> = {
+  "Trainable": { calls: new Set(), declaration: "Trainable" },
+  "MatMul": { calls: new Set(), declaration: "MatMul" },
+  "Bias": { calls: new Set(), declaration: "Bias" }
+}
+
 export function findSize(scope: DeclarationScope, ident: Identifier): Size | undefined {
   return scope.sizes[ident.value];
 }
@@ -28,31 +34,105 @@ export function toValue(scope: DeclarationScope, ident: Identifier): Value {
     nodes: new Set([ident])
   };
 }
- 
+
+function findCircularFlow(flow: Flow): Option<Flow[]>  {
+  const flows: Flow[] = [];
+
+  const visit = (flow: Flow): boolean => {
+    if (flows.includes(flow)) {
+      return true;
+    }
+
+    flows.push(flow);
+
+    for (const callFlow of flow.calls) {
+      if (visit(callFlow)) {
+        return true;
+      }
+    }
+
+    flows.pop();
+
+    return false;
+  }
+
+  if (visit(flow)) {
+    return Some(flows);
+  }
+
+  return None();
+}
+
 export function resolveNames(sourceCode: Declaration[], path: string): Result<FileScope, ResolveError[]> {
   const fileScope: FileScope = {
     path,
-    declarations: {}
+    declarations: {},
+    flows: defaultFlows
   };
 
   const errors: ResolveError[] = [];
+  const names = sourceCode.map(decl => decl.name.value);
 
+  // Initialize file scopes pass
   sourceCode.forEach(decl => {
     const declScope: DeclarationScope = {
       file: fileScope,
       declaration: decl.name.value,
+
+      node: decl,
+      flow: { calls: new Set(), declaration: decl.name.value },
       sizes: {},
       values: {}
     }
 
-    decl.sizeDeclList && decl.sizeDeclList.decls
-      .forEach(size => {
-        declScope.sizes[size.value] = toSize(declScope, size);
+    if (names.filter(name => name === decl.name.value).length > 1) {
+      errors.push({
+        message: `Duplicate function name '${decl.name.value}'.`,
+        node: decl.name
       });
+
+      return;
+    }
+
+    fileScope.declarations[decl.name.value] = declScope;
+    fileScope.flows[decl.name.value] = declScope.flow;
+  });
+
+  // Resolve value names pass
+  sourceCode.forEach(decl => {
+    const declScope = fileScope.declarations[decl.name.value];
+    const identExprs = travel(decl.exprs, isIdentifierExpression);
 
     decl.argumentList.args
       .forEach(arg => {
         declScope.values[arg.ident.value] = toValue(declScope, arg.ident);
+      });
+
+    identExprs
+      .forEach(identExpr => {
+        const value = findValue(declScope, identExpr.ident);
+
+        if (value) {
+          value.nodes.add(identExpr.ident);
+        } else {
+          errors.push({
+            message: `Using undeclared name '${identExpr.ident.value}'.`,
+            node: identExpr
+          });
+        }
+      });
+
+    fileScope.declarations[decl.name.value] = declScope;
+  });
+
+  // Resolve size names pass
+  sourceCode.forEach(decl => {
+    const declScope = fileScope.declarations[decl.name.value];
+    const callExpressions = travel(decl.exprs, isCallExpression);
+
+    decl.sizeDeclList && decl.sizeDeclList.decls
+      .forEach(size => {
+        declScope.sizes[size.value] = toSize(declScope, size);
       });
 
     decl.argumentList.args
@@ -70,24 +150,8 @@ export function resolveNames(sourceCode: Declaration[], path: string): Result<Fi
         } else {
           declScope.sizes[size.value] = toSize(declScope, size);
         }
-      })
-
-    const callExpressions = travel(decl.exprs, isCallExpression);
-    const identExprs = travel(decl.exprs, isIdentifierExpression);
-
-    identExprs
-      .forEach(identExpr => {
-        const value = findValue(declScope, identExpr.ident);
-
-        if (value) {
-          value.nodes.add(identExpr.ident);
-        } else {
-          errors.push({
-            message: `Using undeclared name '${identExpr.ident.value}'.`,
-            node: identExpr
-          });
-        }
       });
+
 
     callExpressions
       .flatMap(sizeDeclList => sizeDeclList.sizes)
@@ -105,9 +169,46 @@ export function resolveNames(sourceCode: Declaration[], path: string): Result<Fi
           });
         }
       });
-
-    fileScope.declarations[decl.name.value] = declScope;
   });
+
+  // Resolve flows pass
+  sourceCode.forEach(decl => {
+    const declFlow = fileScope.flows[decl.name.value];
+    const callExpressions = travel(decl.exprs, isCallExpression);
+
+    callExpressions
+      .forEach(callExpression => {
+        if (callExpression.callee.value === decl.name.value) {
+          errors.push({
+            message: `Recursive call to '${decl.name.value}'.`,
+            node: callExpression.callee
+          });
+        } else if (callExpression.callee.value in fileScope.flows) {
+          const callFlow = fileScope.flows[callExpression.callee.value];
+          declFlow.calls.add(callFlow);
+        } else if (!names.includes(callExpression.callee.value)) {
+          errors.push({
+            message: `Using undeclared flow name '${callExpression.callee.value}'.`,
+            node: callExpression.callee
+          });
+        }
+      });
+  })
+
+  // Check for circular flows
+  for (const flowName in fileScope.flows) {
+    const flow = fileScope.flows[flowName];
+    const result = findCircularFlow(flow);
+
+    if (result.is_some()) {
+      const flows = result.unwrap();
+
+      errors.push({
+        message: `Circular flow detected from '${flows.map(flow => flow.declaration).join(', ')}'.`,
+        node: fileScope.declarations[flows[0].declaration].node
+      });
+    }
+  }
 
   if (errors.length > 0) {
     return Err(errors);
--- packages/nn-type-checker/src/resolver/types.ts
+++ packages/nn-type-checker/src/resolver/types.ts
@@ -1,30 +1,38 @@
 import type { Node } from "nn-language";
 
 export interface FileScope {
-	path: string;
-	declarations: Record<string, DeclarationScope>;
+  path: string;
+  declarations: Record<string, DeclarationScope>;
+  flows: Record<string, Flow>;
 }
 
 export interface DeclarationScope {
-	file: FileScope;
-	declaration: string;
+  file: FileScope;
+  declaration: string;
 
-	sizes: Record<string, Size>;
-	values: Record<string, Value>;
+  flow: Flow;
+  node: Node;
+  sizes: Record<string, Size>;
+  values: Record<string, Value>;
+}
+
+export interface Flow {
+  calls: Set<Flow>;
+  declaration: string;
 }
 
 export interface Size {
-	scope: DeclarationScope;
-	ident: string;
+  scope: DeclarationScope;
+  ident: string;
 
-	nodes: Set<Node>;
+  nodes: Set<Node>;
 }
 
 export interface Value {
-	scope: DeclarationScope;
-	ident: string;
+  scope: DeclarationScope;
+  ident: string;
 
-	nodes: Set<Node>;
+  nodes: Set<Node>;
 }
 
 export interface ResolveError {
